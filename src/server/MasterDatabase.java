2e45fc4ebf2d649133b1bb6ae06db6e4a86947d4
Owen-Tinning <60897087+Owen-Tinning@users.noreply.github.com>
2024-02-29 16:37:45 -0700

    Print Stack trace
    Printing error messages on register and login

--- src/server/MasterDatabase.java
+++ src/server/MasterDatabase.java
@@ -126,7 +126,7 @@ public class MasterDatabase {
             //Return the UserID of the inserted user.
             return getUserID(username);
         }catch(Exception e) {
-        	//e.printStackTrace();;
+        	e.printStackTrace();;
             return -1;
         }
 
@@ -153,6 +153,7 @@ public class MasterDatabase {
             }
 
         }catch(Exception e) {
+        	e.printStackTrace();
             return -1;
         }
 
